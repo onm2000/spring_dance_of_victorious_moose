3a71c569b1affc1259836f9c59bf2b94bc919017
mortonjt <jamietmorton@gmail.com>
2020-03-26 12:38:51 -0400

    converting to torch tesnro

--- src/binding_prediction/language_model.py
+++ src/binding_prediction/language_model.py
@@ -25,4 +25,5 @@ class Elmo(LanguageModel):
 
     def extract(self, x):
         prot = ProteinSequence(x)
-        return self.model.predict(prot.onehot).squeeze()
+        embed = self.model.predict(prot.onehot).squeeze()
+        return torch.Tensor(embed)
