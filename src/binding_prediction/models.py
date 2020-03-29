0b31f837f57f4dddbb1489bcf616cea6ddc091de
omar.mahmood <onm2000@columbia.edu>
2020-03-29 14:35:25 -0400

    fix bugs in Decomposable Attention Model

--- src/binding_prediction/models.py
+++ src/binding_prediction/models.py
@@ -135,9 +135,9 @@ class DecomposableAttentionModel(nn.Module):
         super().__init__()
         self.lm = lm
         self.residue_dim = self.lm.model.output_shape[-1]
-        self.gnn = GraphConv(node_dim, node_dim, activation='ReLU')
+        self.gnn = GraphConv(node_dim, node_dim)
         self.num_gnn_layers = num_gnn_layers
-        self.attn_weight_layer = nn.Sequential(nn.Linear(node_dim, 1), nn.Softmax(dim=-1))
+        self.attn_weight_layer = nn.Sequential(nn.Linear(node_dim+self.residue_dim, 1), nn.Softmax(dim=-1))
         self.interaction_layer = nn.Sequential(
             nn.Linear(node_dim+self.residue_dim, node_dim + int(self.residue_dim/2)),
             nn.ReLU(),
@@ -153,14 +153,16 @@ class DecomposableAttentionModel(nn.Module):
             raise ValueError('Language model is not initialized!')
         protein_sequences = [self.lm(p_i) for p_i in protein_sequences]
         batch_size, max_nodes, node_dim = nodes.shape
-        seq_length, residue_dim = protein_sequences[0].shape[1:]
         batch_graph = build_dgl_graph_batch(nodes, adj_mats)
-        for _ in range(self.num_gnn_layers):
-            nodes = self.gnn(batch_graph)
-            batch_graph.ndata['h'] = nodes
+        nodes = batch_graph.ndata.pop('features')
+        for i in range(self.num_gnn_layers):
+            nodes = self.gnn(batch_graph, nodes)
+            activation = torch.relu if i < self.num_gnn_layers - 1 else torch.tanh
+            nodes = activation(nodes)
         nodes = nodes.reshape(batch_size, max_nodes, -1)
         node_residue_cat = self.merge_graph_w_sequences(nodes, protein_sequences)
-        node_residue_cat = node_residue_cat.reshape(batch_size, max_nodes * seq_length, node_dim + residue_dim)
+        seq_length = node_residue_cat.shape[2]
+        node_residue_cat = node_residue_cat.reshape(batch_size, max_nodes * seq_length, node_dim + self.residue_dim)
         attn_weights = self.attn_weight_layer(node_residue_cat)
         node_residue_interactions = self.interaction_layer(node_residue_cat)
         weighted_sum = (attn_weights * node_residue_interactions).sum(dim=1)
